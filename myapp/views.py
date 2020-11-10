263b42bb226a4fa39b937a4d2a62c239f439e67d
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-10 12:28:12 +0100

    View Simple

--- myapp/views.py
+++ myapp/views.py
@@ -6,3 +6,9 @@ from django.http import HttpResponse
   
 def hello(request):  
     return HttpResponse("<h2>Hello, Welcome to Django!</h2>")  
+
+import datetime  
+def index(request):  
+    now = datetime.datetime.now()  
+    html = "<html><body><h3>Now time is %s.</h3></body></html>" % now  
+    return HttpResponse(html)    
