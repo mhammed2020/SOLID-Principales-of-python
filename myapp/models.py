6b0cac07030b357a0fcffeceb1d6238671bec51b
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-10 14:52:28 +0100

    Create forms.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,13 +1,7 @@
+from __future__ import unicode_literals  
 from django.db import models
-
-# Create your models here.
-# class Employee(models.Model):  
-#     first_name = models.CharField(max_length=30)  
-#     last_name = models.CharField(max_length=30)  
 
 class Student(models.Model):  
     first_name = models.CharField(max_length=20)  
-    last_name  = models.CharField(max_length=30)  
-    contact    = models.IntegerField()  
-    email      = models.EmailField(max_length=50)  
-    age        = models.IntegerField()  
+    last_name  = models.CharField(max_length=30)
+
